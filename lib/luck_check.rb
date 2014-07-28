586c744fee8ef72a2e2e82562b6b1aac577a14b1
Joshua Storey & Dustin Lewis <student@epicodus.com>
2014-07-28 16:22:20 -0700

    passes spec for 5 digit input number

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,9 +1,24 @@
-def luck_check (number)
-  if number.to_s.length != 6
-    true
-  else number_array = number.to_s.split("")
-        first_half = number_array[0..2]
-        # first_half.map(&:to_i).inject(:+)
-        first_half
-  end
+def luck_check(number_string)
+  number_array = number_string.split("").map(&:to_i)
+
+    if number_string.length.odd?
+        # number_array = number_string.split("").to_i
+        number_array.delete_at(number_string.length/2) # e.g., [1,2,2,1]
+        first_half = number_array[0..(number_array.count/2)-1]
+        second_half = number_array[number_array.count/2..-1]
+    else
+        first_half = number_array[0..(number_string.length/2)-1]
+        second_half = number_array[number_string.length/2..-1]
+    end
+
+  first_sum = first_half.inject(:+)
+  second_sum = second_half.inject(:+)
+
+    if first_sum == second_sum
+      true
+    else
+      false
+    end
 end
+
+# puts luck_check(813372)
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -2,11 +2,16 @@ require('rspec')
 require('luck_check')
 
 describe('luck_check') do
-  it('Verifies that the number entered is 6 digits long') do
-    luck_check(12345).should(eq(true))
+  it('compares the sum of the first 3 strings to the second three, returns true if equal') do
+    luck_check("123321").should(eq(true))
   end
 
-  it('splits the first three digits into a separate array') do
-    luck_check(123456).should(eq(["1", "2", "3"]))
+  it('is false if the sum of the first half does not match the sum of the second half') do
+    luck_check("123456").should(eq(false))
   end
+
+  it('is true if the sum of the first 2 numbers and the last 2 numbers of a 5 didgit number are equal') do
+    luck_check("12321").should(eq(true))
+  end
+
 end
